bc85f90702823628dad58ca55ee7e153c6f2c469
Jiqso <keryan.cazaban@gmail.com>
2026-01-15 13:00:56 +0100

    feat(kcf-app) : Update app to take care of translation and react-dom-router in the futur

--- apps/kcf/src/main.tsx
+++ apps/kcf/src/main.tsx
@@ -1,13 +1,30 @@
-import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
-import App from './app/app';
+import { StrictMode, useContext } from "react";
+import * as ReactDOM from "react-dom/client";
+import App from "./app/app";
+import { BrowserRouter } from "react-router-dom";
+import { LanguageContext, LanguageProvider } from "@utils/language-provider";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 
-root.render(
-  <StrictMode>
-    <App/>
-  </StrictMode>
-);
+const AppWrapper: React.FC = () => {
+  const { language, handleLanguageChange } = useContext(LanguageContext);
+
+  return (
+    <StrictMode>
+      <BrowserRouter>
+        <LanguageProvider
+          language={language}
+          handleLanguageChange={handleLanguageChange}
+        >
+          {/* <ThemeProvider theme={mhpTheme}> */}
+          <App />
+          {/* </ThemeProvider> */}
+        </LanguageProvider>
+      </BrowserRouter>
+    </StrictMode>
+  );
+};
+
+root.render(<AppWrapper />);
