d596f963ca21c57926c1a79d4d750e5212003487
Jiqso <keryan.cazaban@gmail.com>
2026-01-15 09:37:38 +0100

    feat(kcf-app) : Update app lib to use packages

--- apps/kcf/src/app/app.tsx
+++ apps/kcf/src/app/app.tsx
@@ -1,15 +1,9 @@
 // Uncomment this line to use CSS modules
-// import styles from './app.module.css';
-import NxWelcome from "./nx-welcome";
+// import styles from './app.module.scss';
 import { FeatureLayout } from "@layout/feature-layout";
 
 export function App() {
-  return (
-    <div>
-      <FeatureLayout />
-      {/* <NxWelcome title="kcf" /> */}
-    </div>
-  );
+  return <FeatureLayout />;
 }
 
 export default App;
