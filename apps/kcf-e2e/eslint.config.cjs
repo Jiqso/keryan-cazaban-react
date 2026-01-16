14fe564f211c664f4f0aae6fe914a1923232556f
Jiqso <keryan.cazaban@gmail.com>
2026-01-16 16:04:50 +0100

    feat(kcf-app): Update eslintconfig.json for  e2e

--- apps/kcf-e2e/eslint.config.cjs
+++ apps/kcf-e2e/eslint.config.cjs
@@ -1,16 +1,13 @@
 const playwright = require("eslint-plugin-playwright");
-const baseConfig = require("../.eslintrc.json");
+const baseConfig = require("../../.eslintrc.json");
 
 module.exports = [
-    playwright.configs["flat/recommended"],
+  playwright.configs["flat/recommended"],
 
-    ...baseConfig,
-    {
-        files: [
-            "**/*.ts",
-            "**/*.js"
-        ],
-        // Override or add rules here
-        rules: {}
-    }
+  ...baseConfig,
+  {
+    files: ["**/*.ts", "**/*.js"],
+    // Override or add rules here
+    rules: {},
+  },
 ];
