f9dc9a0783d53437166ab56084985395303e2d12
Jiqso <keryan.cazaban@gmail.com>
2026-01-27 17:08:14 +0100

    fix(kc): Update app to fix workflow issues

--- packages/kcf/layout/feature-layout/src/lib/feature-layout.spec.tsx
+++ packages/kcf/layout/feature-layout/src/lib/feature-layout.spec.tsx
@@ -1,10 +1,24 @@
 import FeatureLayout from './feature-layout';
 import { IntlHelper } from '@utils/intl-helper';
 import { IntlMessages } from '@utils/translation';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 describe('FeatureLayout', () => {
   it('should render successfully', () => {
-    const { baseElement } = IntlHelper(<FeatureLayout />, 'en', IntlMessages('en'));
+    const queryClient = new QueryClient({
+      defaultOptions: {
+        queries: {
+          retry: false,
+        },
+      },
+    });
+    const { baseElement } = IntlHelper(
+      <QueryClientProvider client={queryClient}>
+        <FeatureLayout />
+      </QueryClientProvider>,
+      'en',
+      IntlMessages('en'),
+    );
     expect(baseElement).toBeTruthy();
   });
 });
