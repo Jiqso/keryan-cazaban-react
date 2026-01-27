027973f86492594d3e4104f7ea9cd170cd465714
Jiqso <keryan.cazaban@gmail.com>
2026-01-27 14:08:01 +0100

    feat(kcb-app): Enable cors

--- apps/kcb/src/main.ts
+++ apps/kcb/src/main.ts
@@ -6,12 +6,22 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000;
+  const configService = app.get(ConfigService);
+
+  // Enable CORS
+  app.enableCors({
+    origin: configService.get<string>('FRONTEND_URL'),
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  const port = configService.get<number>('PORT') || 3000;
   await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}`);
+  Logger.log(`🚀 Application is running on: ${configService.get<string>('HOST_URL')}:${port}`);
 }
 
 bootstrap();
