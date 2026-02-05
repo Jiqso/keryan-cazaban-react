7695f9f8124c5d8f043571cafdba5a3fbdb8e0fa
Jiqso <keryan.cazaban@gmail.com>
2026-02-05 10:29:55 +0100

    feat(kcf-shared): Creating sticky navigation lib

---
+++ packages/shared/navigation/sticky-navigation/src/lib/sticky-navigation.tsx
@@ -0,0 +1,11 @@
+import styles from './sticky-navigation.module.scss';
+
+export function StickyNavigation() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to StickyNavigation!</h1>
+    </div>
+  );
+}
+
+export default StickyNavigation;
