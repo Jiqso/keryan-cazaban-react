aa6d5d9865bf11e41316e973ba132b8f050687ca
John Doe <johndoe@example.com>
2026-02-06 15:46:45 +0100

    fix(kcf-homepage): Update test

--- packages/kcf/homepage/feature-homepage/src/lib/feature-homepage.spec.tsx
+++ packages/kcf/homepage/feature-homepage/src/lib/feature-homepage.spec.tsx
@@ -1,10 +1,10 @@
-import { render } from '@testing-library/react';
-
 import FeatureHomepage from './feature-homepage';
+import { IntlHelper } from '@packages/shared/utils/intl-helper';
+import { IntlMessages } from '@packages/shared/utils/translation';
 
 describe('FeatureHomepage', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<FeatureHomepage />);
+    const { baseElement } = IntlHelper(<FeatureHomepage />, 'en', IntlMessages('en'));
     expect(baseElement).toBeTruthy();
   });
 });
