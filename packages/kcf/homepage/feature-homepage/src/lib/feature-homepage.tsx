8f965dc62f0a15487b5425f3145e229e6bac15ec
John Doe <johndoe@example.com>
2026-02-07 22:00:24 +0100

    fix(kcbfhomepage): Update link to redirect into the marketplace template

--- packages/kcf/homepage/feature-homepage/src/lib/feature-homepage.tsx
+++ packages/kcf/homepage/feature-homepage/src/lib/feature-homepage.tsx
@@ -68,6 +68,7 @@ export function FeatureHomepage() {
     },
   ];
 
+
   const portfolioItems = [
     {
       companyName: 'MyHobbyPlace',
@@ -82,7 +83,7 @@ export function FeatureHomepage() {
       projectTitle: '',
       description: '',
       previewImage: '',
-      websiteUrl: `${import.meta.env.VITE_FRONTEND_URL}/marketplace`,
+      websiteUrl: `/marketplace`,
       tags: ['Marketplace', 'Template'],
     },
   ];
