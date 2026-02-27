c3a719530798364d8e67c903ba23f93c44143efe
Jiqso <keryan.cazaban@gmail.com>
2026-02-27 11:34:21 +0100

    feat(kcf-shared): Update crm navigation test

--- packages/shared/navigation/crm-navigation/src/lib/crm-navigation.spec.tsx
+++ packages/shared/navigation/crm-navigation/src/lib/crm-navigation.spec.tsx
@@ -1,10 +1,10 @@
-import { render } from '@testing-library/react';
-
 import CrmNavigation from './crm-navigation';
+import { IntlHelper } from '@packages/shared/utils/intl-helper';
+import { IntlMessages } from '@packages/shared/utils/translation';
 
 describe('CrmNavigation', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<CrmNavigation />);
+    const { baseElement } = IntlHelper(<CrmNavigation />, 'en', IntlMessages('en'));
     expect(baseElement).toBeTruthy();
   });
 });
