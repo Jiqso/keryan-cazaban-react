f583def227c426fb925a3fd05a2b066b2439c499
Jiqso <keryan.cazaban@gmail.com>
2026-02-27 10:41:51 +0100

    feat(kcf-crm): Create feature dashboard lib

---
+++ packages/kcf/crm/feature-dashboard/src/lib/feature-dashboard.tsx
@@ -0,0 +1,11 @@
+import styles from './feature-dashboard.module.scss';
+
+export function FeatureDashboard() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to FeatureDashboard!</h1>
+    </div>
+  );
+}
+
+export default FeatureDashboard;
