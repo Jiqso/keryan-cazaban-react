d679b419e840a6cb755c4326733853b55866bbee
Jiqso <keryan.cazaban@gmail.com>
2026-02-27 10:40:15 +0100

    feat(kcf-app): Add CRM part to my app

--- apps/kcf/src/app/app.tsx
+++ apps/kcf/src/app/app.tsx
@@ -1,9 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
-import { FeatureHomepage } from '@packages/kcf/homepage/feature-homepage';
-import { FeatureMarketplace } from '@packages/kcf/marketplace/feature-marketplace';
-import { FeatureProduct } from '@packages/kcf/marketplace/feature-product';
+// Navigation
 import { FadedNavigation } from '@packages/shared/navigation/faded-navigation';
 import { StickyNavigation } from '@packages/shared/navigation/sticky-navigation';
+import { CrmNavigation } from '@packages/shared/navigation/crm-navigation';
+
+// Homepage Feature
+import { FeatureHomepage } from '@packages/kcf/homepage/feature-homepage';
+
+// Marketplace Feature
+import { FeatureMarketplace } from '@packages/kcf/marketplace/feature-marketplace';
+import { FeatureProduct } from '@packages/kcf/marketplace/feature-product';
+
+// CRM Feature
+import { FeatureDashboard } from '@packages/kcf/crm/feature-dashboard';
 
 export function App() {
   return (
@@ -14,6 +23,9 @@ export function App() {
       <Route path="/marketplace" element={<StickyNavigation />}>
         <Route index element={<FeatureMarketplace />} />
         <Route path=":id" element={<FeatureProduct />} />
+      </Route>
+      <Route path="/crm" element={<CrmNavigation />}>
+        <Route index element={<FeatureDashboard />} />
       </Route>
     </Routes>
   );
